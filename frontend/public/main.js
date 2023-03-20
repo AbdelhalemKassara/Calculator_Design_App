f0123e39e4b167077f97e120f88a0b076519f5fe
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-03-20 03:45:28 -0400

    setup react and electron app

---
+++ frontend/public/main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow } = require('electron');
+require('@electron/remote/main').initialize();
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+
+function createWindow() {
+  //create the browser window
+  const win = new BrowserWindow({
+    width: 800, height: 600,
+    webPreferences: {
+      enableRemoteModule: true
+    }
+  })
+
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  //if the user quits on mac with cmd + q it won't close the app only the window, so we'll close the app if there aren't any windows
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', function() {
+    //on macos if there are no windows create it
+    if(BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+});
