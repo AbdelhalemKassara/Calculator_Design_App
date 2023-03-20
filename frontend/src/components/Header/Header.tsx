527f9affd183cfd3d8465f512209e4ff196e1352
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-03-20 18:10:04 -0400

    add highlighting which calculator we are currently using

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Styles from './styles.module.css';
 
+
 export default function Header() {
+  const location = useLocation();
+
+  const path = location.pathname;
+
   return (<div className={Styles.header}>
-    <Link to='/infix' className={Styles.link}>Infix</Link>
-    <Link to='/rpn' className={Styles.link}>RPN</Link>
-    <Link to='/brackets' className={Styles.link}>Brackets</Link>
+    <Link to='/infix' className={`${Styles.link} ${path === '/infix' ? Styles.curLink : ''}`}>Infix</Link>
+    <Link to='/rpn' className={`${Styles.link} ${path === '/rpn' ? Styles.curLink : ''}`}>RPN</Link>
+    <Link to='/brackets' className={`${Styles.link} ${path === '/brackets' ? Styles.curLink : ''}`}>Brackets</Link>
   </div>)
 }
