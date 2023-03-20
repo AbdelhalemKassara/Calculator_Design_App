1e2e1960f1178561c586f6340ee44fbb3592afa1
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-03-20 04:23:46 -0400

    add button component and remove unused default assets and code

---
+++ frontend/src/Pages/calulator1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function calulator1() {
+  return (
+    <div>calulator1</div>
+  )
+}
---
+++ frontend/src/components/PageButtons/PageButtons.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './styles.module.css';
+
+export default function PageButtons() {
+  return (<>
+    <div className={styles.buttonContainer}>
+      <button>AC</button>
+      <button>aa</button>
+      <button>a</button>
+      <button>a</button>
+      <button>a</button>
+      <button>a</button>
+      <button>a</button>
+      <button>a</button>
+      <button>a`</button>
+      <button>a</button>
+      <button>a</button>
+    </div>
+
+  </>)
+}
